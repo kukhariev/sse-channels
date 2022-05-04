7adda156813563266dc8a905ef80873e67e012ea
kukhariev <q_h@inbox.ru>
2022-05-04 19:12:12 +0300

    chore: update examples

--- example/index.js
+++ example/index.js
@@ -1,26 +1,26 @@
-'use strict';
-
 const http = require('http');
 const { SseChannels } = require('../lib/');
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
+
 const sse = new SseChannels({ retryInterval: 20000 });
-sse.on('connected', client => {
+
+sse.on('connected', (client) => {
   sse.publish(`+++ ${client.channel} connected (Broadcast Message)`);
 });
-sse.on('disconnected', client => {
+sse.on('disconnected', (client) => {
   sse.publish(`--- ${client.channel} disconnected (Broadcast Message)`);
 });
+
 http
   .createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     let pathname = `.${parsedUrl.pathname}`;
 
-    if (pathname.startsWith('./stream')) {
-      //
+    if (pathname === './stream') {
       sse.subscribe(req, res, parsedUrl.query.ch);
-    } else if (pathname.startsWith('./')) {
+    } else if (pathname === './') {
       pathname = './index.html';
       const data = fs.readFileSync(path.resolve(__dirname, pathname));
       res.setHeader('Content-type', 'text/html');
@@ -33,13 +33,8 @@ http
   .listen(3005);
 
 setInterval(() => {
-  sse.channels.forEach(ch => {
-    sse.publish(
-      ch,
-      `data: ${Math.random().toFixed(2)} channelsCount: ${
-        sse.channels.length
-      } clientsCount: ${sse.connections.length}`
-    );
+  sse.channels.forEach((ch) => {
+    sse.publish(ch, `data: ${Math.random().toFixed(2)} channelsCount: ${sse.channels.length} clientsCount: ${sse.connections.length}`);
   });
 }, 3000);
 console.log('Open http://localhost:3005 in your browser');
